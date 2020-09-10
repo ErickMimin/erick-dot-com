8bd6953acd19b724a75b3f3ba0160b691baaea22
ErickMimin <Eri_mimo@hotmail.com>
2020-09-10 17:13:42 -0500

    Updates

---
+++ precache-manifest.ae1aaffbfd1db9f05dde54e33e05cc62.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a3c8ead4e6b9d9baefffe594de4426",
+    "url": "/erick-dot-com/index.html"
+  },
+  {
+    "revision": "e8e88499e6181379fb49",
+    "url": "/erick-dot-com/static/js/2.30553db5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/erick-dot-com/static/js/2.30553db5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2393a640176c7745ab5",
+    "url": "/erick-dot-com/static/js/main.32df5c72.chunk.js"
+  },
+  {
+    "revision": "ddc781a6d47fe8c03409",
+    "url": "/erick-dot-com/static/js/runtime-main.857a0702.js"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/erick-dot-com/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/erick-dot-com/static/media/OpenSans-Light.2d0bdc8d.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/erick-dot-com/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/erick-dot-com/static/media/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "f9118ee121a613987bb2f42bb8a36272",
+    "url": "/erick-dot-com/static/media/bg.f9118ee1.jpg"
+  },
+  {
+    "revision": "0b3a322da29e5e707290612e269f307d",
+    "url": "/erick-dot-com/static/media/profile.0b3a322d.jpg"
+  },
+  {
+    "revision": "cfc055476bc73976b8f8b925c36ef08f",
+    "url": "/erick-dot-com/static/media/web.cfc05547.jpg"
+  }
+]);
